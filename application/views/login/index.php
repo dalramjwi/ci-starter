821e1a67a54e18368d8cc8f7134cbf63be9b676c
김수현 <dalramjwi@gmail.com>
2025-05-21 17:21:03 +0900

    로그인 화면 css

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,10 +1,12 @@
-<h2><?php echo $title; ?></h2>
-<form action="<?php echo base_url('login/submit'); ?>" method="post">
-    <label>아이디:</label>
-    <input type="text" name="user_id" required><br>
+<p class = "title"><?php echo $title; ?></p>
+<div class="login-form">
+    <form action="<?php echo base_url('login/submit'); ?>" method="post">
+        <label for="user_id">아이디:</label>
+        <input type="text" name="user_id" id="user_id" required>
 
-    <label>비밀번호:</label>
-    <input type="password" name="user_pw" required><br>
+        <label for="user_pw">비밀번호:</label>
+        <input type="password" name="user_pw" id="user_pw" required>
 
-    <button type="submit"><?php echo $title; ?></button>
-</form>
+        <button type="submit"><?php echo $title; ?></button>
+    </form>
+</div>
