a0e9941cf3c47eb479dada1dea915e5ff88e3b0d
김수현 <dalramjwi@gmail.com>
2025-05-21 17:23:13 +0900

    회원가입 화면 css

--- application/views/sign_up/index.php
+++ application/views/sign_up/index.php
@@ -1,9 +1,14 @@
-<form action="<?php echo base_url('sign_up/submit'); ?>" method="post">
-    <label>아이디:</label>
-    <input type="text" name="user_id" required><br>
+<p class = "title"><?php echo $title; ?></p>
+<div class="login-form">
 
-    <label>비밀번호:</label>
-    <input type="password" name="user_pw" required><br>
+    <form action="<?php echo base_url('sign_up/submit'); ?>" method="post">
+        <label>아이디:</label>
+        <input type="text" name="user_id" required><br>
 
-    <button type="submit"><?php echo $title; ?></button>
-</form>
+        <label>비밀번호:</label>
+        <input type="password" name="user_pw" required><br>
+
+        <button type="submit"><?php echo $title; ?></button>
+    </form>
+
+</div>
