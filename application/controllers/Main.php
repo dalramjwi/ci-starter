c4850128ec489bba1b098c841687423496eedb9c
김수현 <dalramjwi@gmail.com>
2025-05-21 09:40:37 +0900

    기존 폴더의 코드 주석 추가

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,17 +6,30 @@ class Main extends MY_Controller
     public function __construct()
     {
         parent::__construct();
-         $this->load->model('Posts_model');
+        
+        // 공통 변수 설정
+        $this->setCommonVars(); 
+        
+        // 게시판 관련 모델 로드
+        $this->load->model('Posts_model');
     }
 
     public function index()
     {
+        // GET, POST, COOKIE에서 넘어온 값 사용 예시
+        // $userName = isset($this->params['name']) ? $this->params['name'] : '익명';
+        
+        // 게시글 불러오기
         $data['title'] = '계층형 게시판 테스트';
-          // 게시글 데이터 불러오기
         $data['posts'] = $this->Posts_model->get_all();
+        // $data['userName'] = $userName;
 
-        // 뷰에 데이터 넘김
-        $this->load->view('templates/header', $data); // 여기서 $title 전달됨
+        // JS, CSS 파일 등록 (필요할 경우)
+        // $this->css('board.css');
+        // $this->js('board.js');
+
+        // 뷰 로드
+        $this->load->view('templates/header', $data);
         $this->load->view('main', $data);
         $this->load->view('templates/footer');
     }
--- application/hooks/post_controller_constructor.php
+++ application/hooks/post_controller_constructor.php
@@ -13,10 +13,10 @@ class post_controller_constructor
     public function init()
     {
         $this->ci =& get_instance();
-
+        //지금 내가 어떤 페이지(컨트롤러+메서드)를 보고 있는지 확인하기 위한 상수 정의
         define('_CONTROLLERS', $this->ci->router->fetch_class());
         define('_METHOD', $this->ci->router->fetch_method());
-
+        //현재 library에 user_agent가 없기에 system의 user_agent를 사용
         $this->ci->load->library("user_agent");
         define('_IS_MOBILE', $this->ci->agent->is_mobile());
     }
