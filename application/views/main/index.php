e606433a03a24e762f128b9119a61a32f251d898
김수현 <dalramjwi@gmail.com>
2025-05-21 18:32:20 +0900

    ajax todo 작성

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -5,22 +5,7 @@
         <?php endif; ?>
     </div>
 </div>
-<?php foreach($posts as $post): ?>
-    <p>
-        <?php
-            // depth만큼 들여쓰기 접두어 만들기 (RE:, RE:RE:, ...)
-            $prefix = str_repeat('RE:', $post->depth);
-
-            // 출력 내용 결정
-            if (!is_null($post->title)) {
-                // 제목이 있으면 링크로 출력
-                echo $prefix . ' <a href="' . base_url('main/view/' . $post->post_id) . '">'
-                            . htmlspecialchars($post->title) . '</a>';
-            } else {
-                // 제목이 없으면 본문 내용 출력
-                                echo $prefix . ' <a href="' . base_url('main/view/' . $post->post_id) . '">'
-                            . htmlspecialchars($post->content) . '</a>';
-            }
-        ?>
-    </p>
-<?php endforeach; ?>
+<!-- select, option을 이용해 event를 발생해 ajax 호출 -->
+<script>
+    //ajax를 사용해 post 형식으로, 전송
+</script>
