70e5273f62f5a88bf78e085025d503a0a36b0317
김수현 <dalramjwi@gmail.com>
2025-05-21 13:29:45 +0900

    wrote fun 추가 및 글작성

--- application/controllers/Write.php
+++ application/controllers/Write.php
@@ -13,9 +13,29 @@ class Write extends MY_Controller
     {
         $data['title'] = '게시판';
         $data['write'] = '글쓰기';
-        //세션으로 user_id값 가져오기, post로 title, content 가져오고 created_at 자동으로 . parent_id, group_id, depth, order_in_group 은 내부 로직으로 넣기 (계층형 게시글  구조)
         $this->load->view('templates/header', $data);
         $this->load->view('write/index', $data);
         $this->load->view('templates/footer');
     }
+    public function wrote() 
+    {
+        $user_id = $this->session->userdata('user_id');
+        $title = $this->input->post('title');
+        $content = $this->input->post('content');
+
+        $data = [
+            'user_id' => $user_id,
+            'title' => $title,
+            'content' => $content,
+            'created_at' => date('Y-m-d H:i:s'),
+            'parent_id' => null,     // 원글이니까 NULL
+            'depth' => 0             // 원글은 depth 0
+        ];
+
+        $this->Posts_model->insert($data);
+
+        redirect('/main');
+    }
+
+
 }
