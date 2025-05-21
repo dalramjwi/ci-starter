c7ec2293aae2f79a95fbcc6b4d6af23596946778
김수현 <dalramjwi@gmail.com>
2025-05-21 17:11:53 +0900

    css 적용 기본

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,15 +2,22 @@
     <head>
         <meta charset="UTF-8">
         <title>계층형 게시판 테스트</title>
+        <link rel="stylesheet" href="<?php echo assets_url();?>css/header.css">
     </head>
     <body>
-    <p><?php echo $title ?></p>
-    <?php if ($this->session->userdata('user_id')): ?>
-    <p><?= $this->session->userdata('user_id') ?>님 환영합니다!</p>
-    <a href="<?php echo base_url('login/logout'); ?>">로그아웃</a>
-<?php else: ?>
-    <a href="<?php echo base_url('login'); ?>">로그인</a>
-    <a href="<?php echo base_url('sign_up'); ?>">회원가입</a>
-<?php endif; ?>
 
 
+<header>
+    <div class="header-left">
+        <a href="<?php echo base_url('/main')?>">계층형 게시판 테스트</a>
+    </div>
+    <div class="header-right">
+        <?php if ($this->session->userdata('user_id')): ?>
+            <span class="user-welcome"><?= $this->session->userdata('user_id') ?>님 환영합니다!</span>
+            <a href="<?php echo base_url('login/logout'); ?>">로그아웃</a>
+        <?php else: ?>
+            <a href="<?php echo base_url('login'); ?>">로그인</a>
+            <a href="<?php echo base_url('sign_up'); ?>">회원가입</a>
+        <?php endif; ?>
+    </div>
+</header>
