b23db985512e7fbc6c87ae7e84d244217c7b5bcd
김수현 <dalramjwi@gmail.com>
2025-05-21 17:55:44 +0900

    게시글 작성 css 적용

--- application/views/write/index.php
+++ application/views/write/index.php
@@ -1,9 +1,12 @@
-<form action="<?php echo base_url('write/wrote'); ?>" method="post">
-    <label>제목:</label>
-    <input type="text" name="title" required><br>
-
-    <label>내용:</label>
-    <textarea name="content" required></textarea><br>
-
-    <button type="submit"><?php echo $write; ?></button>
-</form>
+<div class="write_container">
+    <form id="write_form" action="<?php echo base_url('write/wrote'); ?>" method="post" class="write_form">
+        <label>제목:</label>
+        <input type="text" name="title" required><br>
+        
+        <label>내용:</label>
+        <textarea name="content" required></textarea><br>
+    </form>
+    <div class="write_submit_btn">
+        <button type="submit" form="write_form"><?php echo $write; ?></button>
+    </div>
+</div>
