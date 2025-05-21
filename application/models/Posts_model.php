8cddc21f3e5dc404146f9a86daf9a1a7026cfea9
김수현 <dalramjwi@gmail.com>
2025-05-21 09:41:07 +0900

    core의 MY_Model 활용하는 방법으로 변경

--- application/models/Posts_model.php
+++ application/models/Posts_model.php
@@ -1,11 +1,21 @@
-<?php 
-class Posts_model extends CI_Model {
-  public function __construct()
-  {
-    $this->load->database();
-  }
-  public function get_all()
-  {
-      return $this->db->order_by('order_in_group', 'ASC')->get('posts')->result();
-  }
+<?php
+class Posts_model extends MY_Model
+{
+    protected $table = 'posts';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_all()
+    {
+        $sql = "
+            SELECT * 
+            FROM {$this->table} 
+            ORDER BY order_in_group ASC
+        ";
+
+        return $this->excute($sql, 'rows');
+    }
 }
