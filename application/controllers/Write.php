32a9046ed30ff2b0b4243dd4754a96bacacd209f
김수현 <dalramjwi@gmail.com>
2025-05-23 11:39:19 +0900

    변경된 db 구조에 따라 helper 함수 추가 및 일부 함수 수정

--- application/controllers/Write.php
+++ application/controllers/Write.php
@@ -1,14 +1,16 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Write extends MY_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Posts_model');
+        $this->load->helper('base62_helper');  
     }
 
-    // 게시글 작성 화면을 보여주는 메서드
+    // 게시글 작성 화면 메서드 
     public function index()
     {
         $data['title'] = '게시판';
@@ -17,29 +19,36 @@ class Write extends MY_Controller
         $this->load->view('write/index', $data);
         $this->load->view('templates/footer');
     }
+
+
+    // 게시글 작성 처리 메서드
     public function wrote() 
-{
-    $user_id = $this->session->userdata('user_id');
-    $title = $this->input->post('title');
-    $content = $this->input->post('content');
+    {
+        $user_id = $this->session->userdata('user_id');
+        $title = $this->input->post('title');
+        $content = $this->input->post('content');
 
-    $data = [
-        'user_id' => $user_id,
-        'title' => $title,
-        'content' => $content,
-        'created_at' => date('Y-m-d H:i:s'),
-        'parent_id' => null,
-        'depth' => 0
-    ];
+        // 최상위 글이므로 depth=0
+        $depth = 0;
 
-    // 먼저 insert
-    $insert_id = $this->Posts_model->insert($data);
+        $data = [
+            'user_id' => $user_id,
+            'title' => $title,
+            'content' => $content,
+            'created_at' => date('Y-m-d H:i:s'),
+            'depth' => $depth
+        ];
 
-    // group_id를 자기 자신으로 설정
-    $this->Posts_model->update_post($insert_id, ['group_id' => $insert_id]);
+        // posts 테이블에 새 게시글 삽입
+        $post_id = $this->Posts_model->insert_post($data);
 
-    redirect('/main');
-}
+        // 클로저 테이블에 자기 자신 추가
+        $this->Posts_model->insert_closure($post_id, $post_id, 0);
 
+        // path 테이블에 path 문자열 저장 (최상위 글이므로 path는 base62 인코딩 post_id)
+        $path = base62_encode($post_id);
+        $this->Posts_model->insert_path($post_id, $path);
 
+        redirect('/main');
+    }
 }
--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -6,4 +6,30 @@ function assets_url() {
     return base_url() . 'assets/';
 }
 
-?>
+function base62_encode($num) {
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $base = 62;
+    $result = '';
+    if ($num == 0) return '0';
+    while ($num > 0) {
+        $result = $chars[$num % $base] . $result;
+        $num = intval($num / $base);
+    }
+    return $result;
+}
+
+function base62_decode($str) {
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $base = 62;
+    $length = strlen($str);
+    $num = 0;
+    for ($i = 0; $i < $length; $i++) {
+        $pos = strpos($chars, $str[$i]);
+        if ($pos === false) {
+            return false;
+        }
+        $num = $num * $base + $pos;
+    }
+    return $num;
+}
+
