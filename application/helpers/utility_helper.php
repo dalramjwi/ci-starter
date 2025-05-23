fee768162ffc36cd4b107b6e966be2b4df05abf5
김수현 <dalramjwi@gmail.com>
2025-05-23 13:15:53 +0900

    고정 길이 패딩 처리 추가

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -6,17 +6,24 @@ function assets_url() {
     return base_url() . 'assets/';
 }
 
-function base62_encode($num) {
+function base62_encode($num, $pad_length = 4) {
     $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $base = 62;
     $result = '';
-    if ($num == 0) return '0';
-    while ($num > 0) {
-        $result = $chars[$num % $base] . $result;
-        $num = intval($num / $base);
+    
+    if ($num == 0) {
+        $result = '0';
+    } else {
+        while ($num > 0) {
+            $result = $chars[$num % $base] . $result;
+            $num = intval($num / $base);
+        }
     }
-    return $result;
+    
+    // 고정 길이 패딩 처리
+    return str_pad($result, $pad_length, '0', STR_PAD_LEFT);
 }
+
 
 function base62_decode($str) {
     $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
