c7fdeaf0067be8e92740401c33eb68666187c4f5
김수현 <dalramjwi@gmail.com>
2025-05-23 14:51:02 +0900

    정렬을 위한 group_id 추가에 따른 글 작성시 group_id 추가

--- application/controllers/Write.php
+++ application/controllers/Write.php
@@ -22,7 +22,6 @@ class Write extends MY_Controller
         $this->load->view('templates/footer');
     }
 
-
     public function wrote()
     {
         $user_id = $this->session->userdata('user_id');
@@ -35,16 +34,20 @@ class Write extends MY_Controller
             'title' => $title,
             'content' => $content,
             'created_at' => date('Y-m-d H:i:s'),
-            'depth' => 0
+            'depth' => 0,
+            'group_id' => 0  // 일단 0으로 처리 후 후처리
         ];
 
         // posts 테이블에 저장 및 새 post_id 받기
         $insert_id = $this->Posts_model->insert($data);
 
+        // insert_id로 group_id 업데이트
+        $this->Posts_model->update_group_id($insert_id, $insert_id);
+
         // 클로저 테이블에 자기 자신 관계 (depth 0)
         $this->Posts_closure_model->insert($insert_id, $insert_id, 0);
 
-        // path 경로는 post_id를 base62로 인코딩해서 문자열 생성 (최상위 글라서 단일 path)
+        // path 경로는 post_id를 base62로 인코딩해서 문자열 생성 (최상위 글이라서 단일 path)
         $path = base62_encode($insert_id);
 
         // path 테이블에 저장
@@ -52,4 +55,5 @@ class Write extends MY_Controller
 
         redirect('/main');
     }
+
 }
