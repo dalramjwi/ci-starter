f09289fd5cbc01c3b79de6bb95461aa3113f13b3
김수현 <dalramjwi@gmail.com>
2025-05-29 13:50:26 +0900

    주석 설명 추가

--- application/controllers/Start.php
+++ application/controllers/Start.php
@@ -1,10 +1,21 @@
 <?php
-class Start extends MY_Controller {
+/**
+ * Start 컨트롤러
+ *
+ * 사이트의 시작(인트로) 화면을 보여주는 단순한 컨트롤러입니다.
+ * 실제 기능은 없고, 디자인이 중심인 첫 화면을 렌더링합니다.
+ *
+ * 주요 기능:
+ * - index(): 시작화면 뷰(start/index)를 출력합니다.
+ */
+class Start extends MY_Controller
+{
         public function __construct()
         {
                 parent::__construct();
                 $this->setCommonVars();
         }
+
         public function index()
         {
                 $this->load->view('start/index');
