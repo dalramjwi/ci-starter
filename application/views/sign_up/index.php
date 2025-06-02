5d5fcd7e158fbc4af7777f55e76b3f99210fd279
김수현 <dalramjwi@gmail.com>
2025-06-02 15:36:11 +0900

    로그인 회원가입 ui 통일

--- application/views/sign_up/index.php
+++ application/views/sign_up/index.php
@@ -1,15 +1,13 @@
 <?php $this->load->view('common/toast'); ?>
 
 <p class = "title"><?php echo $sign_up; ?></p>
-
 <div class="login-form">
-
     <form action="<?php echo base_url('sign_up/submit'); ?>" method="post">
-        <label>아이디:</label>
-        <input type="text" name="user_id" required><br>
+        <label for="user_id">아이디:</label>
+        <input type="text" name="user_id" id="user_id" required>
 
-        <label>비밀번호:</label>
-        <input type="password" name="user_pw" required><br>
+        <label for="user_pw">비밀번호:</label>
+        <input type="password" name="user_pw" id="user_pw" required>
 
         <button type="submit"><?php echo $sign_up; ?></button>
     </form>
